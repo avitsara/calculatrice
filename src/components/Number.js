06997dfac01f30b887ed7f7b21fa4fae50d18068
avitsara <avitsarajary@yahoo.com>
2020-12-21 23:28:46 +0100

    modification listener sur Number

--- src/components/Number.js
+++ src/components/Number.js
@@ -12,43 +12,46 @@ class Number extends React.Component {
         this.handleClickNumber = this.handleClickNumber.bind(this);
     }
     // 
-    handleClickNumber(){
-        console.log("click sur un number");
+    handleClickNumber(e){
+        ///console.log("click sur un number");
+        console.log(e.target.value); 
     }
     render(){
         return (
             <div>
             <div className="row">
+                
                 <div className="col-xs-4">
-                    <button type="button" className="number" onClick={this.handleClickNumber}>1</button>
+                    <button type="button" className="number"  value="1" onClick={this.handleClickNumber}>1</button>
                 </div>
                     <div className="col-xs-4">
-                        <button type="button" className="number" onClick={this.handleClickNumber}>2</button>
+                        <button type="button" className="number" value="2" onClick={this.handleClickNumber}>2</button>
                     </div>
                     <div className="col-xs-4">
-                        <button type="button" className="number" onClick={this.handleClickNumber}>3</button>
+                        <button type="button" className="number" value="3" onClick={this.handleClickNumber}>3</button>
                     </div>
                 </div>
                     <div className="row">
+       
                         <div className="col-xs-4">
-                          <button type="button" className="number" onClick={this.handleClickNumber}>4</button>
+                          <button type="button" className="number" value="4" onClick={this.handleClickNumber}>4</button>
                         </div>
                         <div className="col-xs-4">
-                          <button type="button" className="number" onClick={this.handleClickNumber}>5</button>
+                          <button type="button" className="number" value="5" onClick={this.handleClickNumber}>5</button>
                         </div>
                         <div className="col-xs-4">
-                          <button type="button" className="number" onClick={this.handleClickNumber}>6</button>
+                          <button type="button" className="number" value="6" onClick={this.handleClickNumber}>6</button>
                         </div>
                     </div>
                     <div className="row">
                           <div className="col-xs-4">
-                              <button type="button" className="number" onClick={this.handleClickNumber}>7</button>
+                              <button type="button" value="7" className="number" onClick={this.handleClickNumber}>7</button>
                           </div>
                           <div className="col-xs-4">
-                              <button type="button" className="number" onClick={this.handleClickNumber}>8</button>
+                              <button type="button" value="8" className="number" onClick={this.handleClickNumber}>8</button>
                           </div>
                           <div className="col-xs-4">
-                              <button type="button" className="number" onClick={this.handleClickNumber}>9</button>
+                              <button type="button" value="9" className="number" onClick={this.handleClickNumber}>9</button>
                           </div>                       
                     </div>
                 </div>
